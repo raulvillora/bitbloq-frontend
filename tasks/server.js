d6df4bf77e0bbe662d61ef9c44da159f110a6b7b
Tom Calvo <calvo.tom@gmail.com>
2016-05-05 16:57:36 +0200

    server.js to upload database files , fix faqs and bloqs

--- tasks/server.js
+++ tasks/server.js
@@ -1,53 +1,112 @@
 module.exports = function(grunt) {
 
-    function getToken(env, callback) {
-        console.log('Get token on ' + env);
-        var http = require('http'),
-            configFile = grunt.file.readJSON('./app/res/config/' + env + '/config.json'),
-            postOptions = {
+    var http = require('http');
+
+    function IsJsonString(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
+    function requestToServer(env, method, endPoint, headers, data, callback) {
+        var configFile = grunt.file.readJSON('./app/res/config/' + env + '/config.json'),
+            options = {
                 hostname: configFile.serverHost,
                 port: configFile.serverPort,
-                path: configFile.serverPath + 'auth/local',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            },
-            postData = {
-                email: configFile.adminUser,
-                password: configFile.adminPassword
+                path: configFile.serverPath + endPoint,
+                method: method
             };
+        data = JSON.stringify(data);
+        options.headers = headers || {};
+        options.headers['Content-Type'] = 'application/json';
+        options.headers['Content-Length'] = Buffer.byteLength(data);
+        //console.log(configFile.adminUser, configFile.adminPassword);
+        //console.log('options.headers[Content-Length]', options.headers['Content-Length']);
 
-        var postRequest = http.request(postOptions, function(res) {
+        var postRequest = http.request(options, function(res) {
             res.on('data', function(chunk) {
-                // console.log('Received body data:');
+                // console.log('Received body data:', res.statusCode);
                 // console.log(JSON.parse(chunk.toString()).token);
-                callback(null, JSON.parse(chunk.toString()).token);
+                //console.log('res.statusCode', res.statusCode);
+                if ((res.statusCode >= 200) && (res.statusCode < 300)) {
+                    var result = chunk.toString();
+                    if (IsJsonString(result)) {
+                        result = JSON.parse(result);
+                    }
+                    callback(null, result);
+                } else {
+                    callback(res.statusCode);
+                }
             });
         });
 
         postRequest.on('error', function(err) {
-            console.log('problem with request: ${err.message}');
+            console.log('problem with request:', err.message);
             callback(err);
         });
 
-        postRequest.write(JSON.stringify(postData));
+        postRequest.write(data);
         postRequest.end();
     };
 
-    function refreshServerCollection(token, collectionName, items, env, setId, callback) {
+    function adminRequestToServer(env, method, endPoint, data, callback) {
+        getToken(env, function(err, res) {
+            if (err) {
+                callback(err);
+            } else {
+                //console.log(res.token);
+                var headers = {
+                    'Authorization': 'Bearer ' + res.token
+                }
+                requestToServer(env, method, endPoint, headers, data, callback);
+            }
+        });
+    };
+
+    function getToken(env, callback) {
+        var configFile = grunt.file.readJSON('./app/res/config/' + env + '/config.json'),
+            data = {
+                email: configFile.adminUser,
+                password: configFile.adminPassword
+            };
+        requestToServer(env, 'POST', 'auth/local', null, data, callback);
+    };
+
+    function deleteCollection(collectionName, env, callback) {
+        adminRequestToServer(env, 'DELETE', collectionName + '/all', {}, callback);
+    };
+
+    function insertCollection(collectionName, items, env, callback) {
+        adminRequestToServer(env, 'POST', collectionName + '/all', items, callback);
+    };
+
+    function refreshServerCollection(collectionName, items, env, setId, callback) {
         console.log('refresh collection on ' + env);
-        deleteCollection();
-        insertCollection();
-        callback();
+        deleteCollection(collectionName, env, function(err) {
+            if (err) {
+                callback(err);
+            } else {
+                console.log('deleted now start to insert');
+                insertCollection(collectionName, items, env, callback);
+            }
+        });
     };
 
     grunt.registerTask('updateCollection', function(collectionName, env, setId) {
         grunt.log.writeln('Updating= ' + collectionName + ' on ' + env + ' setting the ID:' + setId);
         var done = this.async(),
             items = grunt.file.readJSON('dataBaseFiles/' + collectionName + '/' + collectionName + '.json');
-        getToken(env, function(err, token) {
-            refreshServerCollection(token, collectionName, items, env, setId, done);
+        refreshServerCollection(collectionName, items, env, setId, function(err, res) {
+            if (err) {
+                console.log('err updating ', collectionName, 'on', env, ':', err);
+                done();
+            } else {
+                console.log(collectionName, 'on', env, 'update OK');
+                done();
+            }
         });
     });
 };
