187125c76fdfc2a5008e5d02889013f8b07a4e03
Tom Calvo <calvo.tom@gmail.com>
2016-05-05 13:26:03 +0200

    get token on server task done

--- tasks/server.js
+++ tasks/server.js
@@ -1,9 +1,53 @@
 module.exports = function(grunt) {
-    grunt.registerTask('updateCollection', function(collection, env, setId) {
-        grunt.log.writeln('Updating= ' + collection + ' on ' + env + ' setting the ID:' + setId);
+
+    function getToken(env, callback) {
+        console.log('Get token on ' + env);
+        var http = require('http'),
+            configFile = grunt.file.readJSON('./app/res/config/' + env + '/config.json'),
+            postOptions = {
+                hostname: configFile.serverHost,
+                port: configFile.serverPort,
+                path: configFile.serverPath + 'auth/local',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            },
+            postData = {
+                email: configFile.adminUser,
+                password: configFile.adminPassword
+            };
+
+        var postRequest = http.request(postOptions, function(res) {
+            res.on('data', function(chunk) {
+                // console.log('Received body data:');
+                // console.log(JSON.parse(chunk.toString()).token);
+                callback(null, JSON.parse(chunk.toString()).token);
+            });
+        });
+
+        postRequest.on('error', function(err) {
+            console.log('problem with request: ${err.message}');
+            callback(err);
+        });
+
+        postRequest.write(JSON.stringify(postData));
+        postRequest.end();
+    };
+
+    function refreshServerCollection(token, collectionName, items, env, setId, callback) {
+        console.log('refresh collection on ' + env);
+        deleteCollection();
+        insertCollection();
+        callback();
+    };
+
+    grunt.registerTask('updateCollection', function(collectionName, env, setId) {
+        grunt.log.writeln('Updating= ' + collectionName + ' on ' + env + ' setting the ID:' + setId);
         var done = this.async(),
-            item = grunt.file.readJSON('dataBaseFiles/' + collection + '/' + collection + '.json');
-        //refreshCorbelCollection('bitbloq:' + collection, item, env, setId, done);
-        done();
+            items = grunt.file.readJSON('dataBaseFiles/' + collectionName + '/' + collectionName + '.json');
+        getToken(env, function(err, token) {
+            refreshServerCollection(token, collectionName, items, env, setId, done);
+        });
     });
 };
