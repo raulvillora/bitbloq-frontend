1d2817869ce6efd1cbe993caf8a399437b674cd0
Tom Calvo <calvo.tom@gmail.com>
2016-06-20 09:19:53 +0200

    add xlink namespace on svg root to allow use of images an references.

--- Gruntfile.js
+++ Gruntfile.js
@@ -345,7 +345,8 @@ module.exports = function(grunt) {
             options: {
                 svg: {
                     viewBox: '0 0 100 100',
-                    xmlns: 'http://www.w3.org/2000/svg'
+                    xmlns: 'http://www.w3.org/2000/svg',
+                    'xmlns:xlink':'http://www.w3.org/1999/xlink'
                 },
                 includedemo: true,
                 formatting: {
