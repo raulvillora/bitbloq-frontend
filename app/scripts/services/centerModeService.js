78e80fc1e0c0d3b6f3e686464d3f72e788f19283
LauraDRA <laura.delrio.aviles@gmail.com>
2017-02-14 11:30:36 +0100

    [SWBIT-2034] new group modal

--- app/scripts/services/centerModeService.js
+++ app/scripts/services/centerModeService.js
@@ -43,7 +43,7 @@ angular.module('bitbloqApp')
                     placeholder: 'centerMode_modal_createGroupPlaceholder'
                 },
                 condition: function() {
-                    return this.input.value;
+                    return this.input.value && centerId;
                 },
                 confirmButton: 'centerMode_button_createGroup',
                 rejectButton: 'modal-button-cancel',
@@ -56,20 +56,25 @@ angular.module('bitbloqApp')
                 centerArray = [];
                 centerModeApi.getMyCentersAsTeacher().then(function(response) {
                     centerArray = response.data;
-                    _.extend(modalOptions, {
-                        modalDropdown: true,
-                        modaloptions: centerArray,
-                        optionsClick: function(selected) {
-                            centerId = selected._id;
-                        },
-                        headingOptions: 'centerMode_createGroup_selectCenter'
-                    });
+                    if (centerArray.length === 1) {
+                        centerId = centerArray[0];
+                    } else {
+                        _.extend(modalOptions, {
+                            modalDropdown: true,
+                            modaloptions: centerArray,
+                            optionsClick: function(selected) {
+                                centerId = selected._id;
+                            },
+                            headingOptions: 'centerMode_createGroup_selectCenter'
+                        });
+                    }
                     ngDialog.open({
                         template: '/views/modals/modal.html',
                         className: 'modal--container modal--input',
                         scope: modalOptions
 
                     });
+
                 });
             } else {
                 ngDialog.open({
