7ce344addc86219b068f4daa2d2ab156199cefb7
Álvaro Sánchez Bravo <alvaro.sanchez@bq.com>
2017-04-21 10:42:43 +0200

    Fixed jshint in moveNextOnMaxlength

--- app/scripts/directives/moveNextOnMaxlength.js
+++ app/scripts/directives/moveNextOnMaxlength.js
@@ -1,4 +1,5 @@
 /*jshint camelcase: false */
+/* jshint ignore:start */
 'use strict';
 
 /**
@@ -21,3 +22,4 @@ angular.module('bitbloqApp')
             }
         };
     });
+/* jshint ignore:end */
