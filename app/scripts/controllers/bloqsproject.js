6e953f6b543974b8a67c283d908f07a728815025
LauraDRA <laura.delrio.aviles@gmail.com>
2017-05-04 17:31:22 +0200

    [SWBIT-1929] fix: skip step tour

--- app/scripts/controllers/bloqsproject.js
+++ app/scripts/controllers/bloqsproject.js
@@ -10,8 +10,9 @@
 
 angular.module('bitbloqApp')
     .controller('BloqsprojectCtrl', function($rootScope, $route, $scope, $log, $timeout, $routeParams, $document, $window, $location,
-        $q, web2board, alertsService, ngDialog, _, projectApi, bloqs, bloqsUtils, utils, userApi, hw2Bloqs, web2boardOnline, commonModals,
-        projectService, hardwareConstants, chromeAppApi) {
+                                             $q, web2board, alertsService, ngDialog, _, projectApi, bloqs, bloqsUtils, utils, userApi, hw2Bloqs, web2boardOnline, commonModals,
+                                             projectService, hardwareConstants, chromeAppApi)
+    {
 
         /*************************************************
          Project save / edit
@@ -831,17 +832,17 @@ angular.module('bitbloqApp')
          UNDO / REDO
          *************************************************/
 
-        //Stores one step in the history
+            //Stores one step in the history
         $scope.saveBloqStep = function(step) {
             //$log.debug('Guardamos Estado de Bloqs');
             var freeBloqs = bloqs.getFreeBloqs();
             //$log.debug(freeBloqs);
             step = step || {
-                vars: projectService.bloqs.varsBloq.getBloqsStructure(),
-                setup: projectService.bloqs.setupBloq.getBloqsStructure(),
-                loop: projectService.bloqs.loopBloq.getBloqsStructure(),
-                freeBloqs: freeBloqs
-            };
+                    vars: projectService.bloqs.varsBloq.getBloqsStructure(),
+                    setup: projectService.bloqs.setupBloq.getBloqsStructure(),
+                    loop: projectService.bloqs.loopBloq.getBloqsStructure(),
+                    freeBloqs: freeBloqs
+                };
             //showProjectResumeOnConsole(step);
             if ($scope.bloqsHistory.pointer !== ($scope.bloqsHistory.history.length - 1)) {
                 $scope.bloqsHistory.history = _.take($scope.bloqsHistory.history, $scope.bloqsHistory.pointer + 1);
@@ -1015,7 +1016,8 @@ angular.module('bitbloqApp')
             if (event.which === 8 &&
                 event.target.nodeName !== 'INPUT' &&
                 event.target.nodeName !== 'SELECT' &&
-                event.target.nodeName !== 'TEXTAREA' && !$document[0].activeElement.attributes['data-bloq-id']) {
+                event.target.nodeName !== 'TEXTAREA' && !$document[0].activeElement.attributes['data-bloq-id'])
+            {
 
                 event.preventDefault();
             }
@@ -1050,11 +1052,16 @@ angular.module('bitbloqApp')
                     break;
                 case 3:
                     if ($scope.tourCurrentStep === 2) {
-                        $scope.tourCurrentStep = 3;
-                        var runStepFour = $rootScope.$on('component-connected', function() {
-                            $scope.handleTour(4);
-                            runStepFour();
-                        });
+                        if (!$scope.currentProject.hardware.board) {
+                            $scope.tourCurrentStep === 1;
+                            $scope.handleTour(2);
+                        } else {
+                            $scope.tourCurrentStep = 3;
+                            var runStepFour = $rootScope.$on('component-connected', function() {
+                                $scope.handleTour(4);
+                                runStepFour();
+                            });
+                        }
                     }
                     break;
                 case 4:
