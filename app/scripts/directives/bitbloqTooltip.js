ac5c576552a14fa181ff684c9d9d33b09eb140d6
Tom Calvo <calvo.tom@gmail.com>
2017-07-11 16:11:37 +0200

    added tooltips directive, added tooltip on center mode, update bloqs to add pinMode on digitalWritte.

---
+++ app/scripts/directives/bitbloqTooltip.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name bitbloqApp.directive:BitbloqTooltip
+ * @description
+ * # BitbloqTooltip
+ */
+
+angular.module('bitbloqApp')
+    .directive('bitbloqTooltip', function($log, utils) {
+        return {
+            restrict: 'A',
+            scope: {
+                tooltip: '@',
+                tooltipPosition: '@',
+                tooltips: '@'
+            },
+
+            link: function(scope, elem, attr) {
+                var widthUsedPerChar = 8;
+                //console.log("im a tooltip!", scope, elem, attr);
+                if (attr['bitbloqTooltipShowonellipsis']) {
+                    elem[0].setAttribute('data-tooltips', false);
+
+                    scope.$watchGroup([
+                        function() {
+                            return elem[0].offsetWidth;
+                        },
+                        function() {
+                            return elem[0].offsetHeight;
+                        }
+                    ], function(values) {
+
+                        var numCharacters = elem[0].getAttribute('data-tooltip').length;
+                        var needTooltip = elem[0].offsetWidth <= (numCharacters * widthUsedPerChar);
+                        console.log('resize event!!', numCharacters, needTooltip, elem[0].offsetWidth);
+                        if (needTooltip) {
+                            elem[0].setAttribute('data-tooltips', true);
+                        } else {
+                            elem[0].setAttribute('data-tooltips', false);
+                        }
+                    });
+                }
+
+            }
+        };
+    });
