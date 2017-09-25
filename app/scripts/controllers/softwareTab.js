bb220699df28ad480ed51cc329d52e901a31adab
Tom Calvo <calvo.tom@gmail.com>
2017-09-25 19:21:08 +0200

    update bloqs to add new make block bloqs

--- app/scripts/controllers/softwareTab.js
+++ app/scripts/controllers/softwareTab.js
@@ -448,6 +448,8 @@ angular.module('bitbloqApp')
                         result = existComponent(['mkb_display7seg'], connectedComponents);
                     } else if ((item === 'freakscarBuzzer') || (item === 'freakscarDistance') || (item === 'freakscarLight')) {
                         result = existComponent(['freakscar_integrated_lightsensor'], connectedComponents);
+                    } else if ((item === 'mkbGyroscope') || (item === 'mkbIntegratedSoundSensor')) {
+                        result = currentProjectService.project && currentProjectService.project.hardware && (currentProjectService.project.hardware.board === 'meauriga');
                     } else if ((item === 'robotSetMotorSpeed') || (item === 'robotSetMotorSpeedAdvanced')) {
                         if (currentProjectService.project && currentProjectService.project.hardware) {
                             switch (currentProjectService.project.hardware.board) {
