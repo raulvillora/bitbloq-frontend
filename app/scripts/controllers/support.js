c6b140f0396bf99c03a34c7a15db65975f470c2c
Alvaro Almendros Gala <alvaroalmendros@me.com>
2017-10-14 18:42:30 +0200

    fix: support navigation between cards
    [SWBIT-2546]
    added: button logic for navigation between routes
    added: home button -> support index 
    fixed: style improvements
    fixed: package-lock.json to the .gitignore

--- app/scripts/controllers/support.js
+++ app/scripts/controllers/support.js
@@ -11,67 +11,67 @@ angular.module('bitbloqApp')
     .controller('SupportCtrl', function($scope, $location, $routeParams) {
         var db = [
           {
-            '_id': 0,
+            '_id': '0',
             'title': 'Indice de FAQ',
             'data': '',
             'next': [{
-                    '_id': 1,
+                    '_id': '1',
                     'class': 'btn--secondary',
                     'response': 'default short'
                 }, {
-                    '_id': 3,
+                    '_id': '3',
                     'class': 'btn--primary',
                     'response': 'si'
                 }, {
-                    '_id': 100,
+                    '_id': '100',
                     'class': 'btn--secondary',
                     'response': 'default very very very very very long'
                 }, {
-                    '_id': 1000,
+                    '_id': '1000',
                     'class': 'btn--primary btn--no',
                     'response': 'no'
                 }]
           }, {
-              '_id': 1,
+              '_id': '1',
               'title': '¿La web no carga y/o aparece en blanco?',
               'data': '',
               'next': [{
-                      '_id': 2,
+                      '_id': '2',
                       'class': 'btn--primary',
                       'response': 'si',
                   },
                   {
-                      '_id': 3,
+                      '_id': '3',
                       'response': 'no',
                       'class': 'btn--primary btn--no'
                   }
               ]
           }, {
-              '_id': 2,
+              '_id': '2',
               'title': '¿Ha refrescado la caché?',
               'data': '',
               'next': [{
-                      '_id': 1,
+                      '_id': '1',
                       'class': 'btn--primary',
                       'response': 'si',
                   },
                   {
-                      '_id': 3,
+                      '_id': '3',
                       'response': 'no',
                       'class': 'btn--primary btn--no'
                   }
               ]
           }, {
-              '_id': 3,
+              '_id': '3',
               'title': '¿Se instala web2board?',
               'data': '',
               'next': [{
-                      '_id': 1,
+                      '_id': '1',
                       'class': 'btn--primary',
                       'response': 'si',
                   },
                   {
-                      '_id': 2,
+                      '_id': '2',
                       'response': 'no',
                       'class': 'btn--primary btn--no'
                   }
@@ -79,17 +79,19 @@ angular.module('bitbloqApp')
           }];
 
         if ($routeParams.id !== undefined) {
-          console.log($routeParams.id, typeof $routeParams.id);
-          console.log(db);
-          console.log(db.filter(card => card._id === $routeParams.id));
-          console.log(db.filter(card => card._id === $routeParams.id).pop());
             $scope.card = db.filter(card => card._id === $routeParams.id).pop();
+            $scope.show_back = true;
         } else {
-            $scope.card = db.filter(card => card._id === 0).pop();
+            $scope.card = db.filter(card => card._id === '0').pop();
+            $scope.show_back = false;
         }
 
         $scope.go = function(childId) {
-            $location.path($location.path() + childId);
+          if(childId) {
+            $location.path(`/support/${childId}`);
+          } else {
+            $location.path(`/support/`);
+          }
         };
         console.log($scope.card);
     });
