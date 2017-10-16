9d8d232ae1d4e691d806e3769a1e3d3773f205e4
Alvaro Almendros Gala <alvaroalmendros@me.com>
2017-10-16 18:04:46 +0200

    [SWBIT-2546] fix: support external templates

--- app/scripts/controllers/support.js
+++ app/scripts/controllers/support.js
@@ -11,116 +11,155 @@ angular.module('bitbloqApp')
     .controller('SupportCtrl', function($scope, $location, $routeParams, common) {
         var db = [
           {
-            '_id': 'Support--Index',
-            'idNum': '1',
+            '_id': 'index',
+            'default': 'index',
+            'dontShowHomeButton': true,
             'title': '¡Bienvenido a la página de soporte de Bitbloq!',
             'data': '<p>Ayudanos a diagnosticar tu caso para que podamos ayudarte.</p><p>¿Usas la <strong>versión Online</strong> de Bitbloq, o la versión <a href="http://bitbloq.bq.com/#/offline" class="icon--url">Offline</a></p>',
             'next': [{
-                    '_id': 'Support--Online',
+                    '_id': 'online',
                     'class': 'btn--secondary',
                     'icon': 'icon--cloud icon--big',
                     'response': 'Uso la versión Online'
                 }, {
-                    '_id': 'Support--Offline',
+                    '_id': 'offline',
                     'class': 'btn--secondary',
                     'icon': 'icon--desktop icon--big',
                     'response': 'Uso la versión Offline'
                 }]
           }, {
-            '_id': 'Support--End',
+            '_id': 'end',
+            'default': 'end',
+            'dontShowHomeButton': true,
             'title': '¡Gracias por utilizar el sistema de soporte de Bitbloq!',
             'data': '<span class="support--icon--giga support--icon--ok"><i class="fa fa-check-circle" aria-hidden="true"></i><span>',
             'next': [{
-                    '_id': 'Support--Index',
+                    '_id': 'index',
                     'class': 'btn--primary',
                     'icon': 'icon--home icon--big',
                     'response': 'Volver a el índice'
                 }]
           }, {
-            '_id': 'Support--404',
+            '_id': '404',
+            'default': '404',
+            'dontShowHomeButton': true,
             'title': 'No encuentro la página de soporte que buscas...',
-            'data': 'Puede que exista un error; Inténtalo de nuevo<br><span class="support--icon--giga support--icon--no"><i class="fa fa-times-circle" aria-hidden="true"></i><span>',
+            'data': 'Puede que exista un error; Inténtalo de nuevo<br><span class="support--icon--giga support--icon--no"><svg class="svg-icon"><use xlink:href="#warning"></use></svg><span>',
             'next': [{
-                    '_id': 'Support--Index',
+                    '_id': 'index',
                     'class': 'btn--primary',
                     'icon': 'icon--home icon--big',
                     'response': 'Volver a el índice'
                 }]
           }, {
-              '_id': 'Support--Online',
+            '_id': 'form',
+            'default': 'form',
+            'dontShowHomeButton': true,
+            'title': 'Contacta con nuestro servicio técnico',
+            'extData': 'contactForm.html',
+            'next': []
+          }, {
+              '_id': 'online',
               'title': 'Por favor, indicanos el motivo de tu consulta:',
-              'data': '',
               'next': [{
-                      '_id': 'Support--DontLoad',
+                      '_id': 'dontLoad',
                       'class': 'btn--secondary',
                       'icon': '',
                       'response': 'La web no carga',
                   }, {
-                      '_id': 'Support--W2B',
+                      '_id': 'w2b',
                       'class': 'btn--secondary',
                       'icon': '',
                       'response': 'Tengo dificultades con web2board',
                   }, {
-                      '_id': 'Support--Hardware',
+                      '_id': 'hardware',
                       'class': 'btn--secondary',
                       'icon': '',
                       'response': 'Tengo una incidencia con un componente de hardware',
                   }
               ]
           }, {
-              '_id': 'Support--Offline',
+              '_id': 'offline',
               'title': '',
               'data': '',
               'next': []
           }, {
-              '_id': 'Support--DontLoad',
+              '_id': 'dontLoad',
               'title': '¿Tienes problemas cargando la web de Bitbloq?',
-              'data': '<p>¿Has <strong>refrescado la caché</strong> del navegador?</p><p>La caché del navegador es una función que tienen los principales navegadores web, que permite cargar las páginas web más rápido guardando en el ordenador parte de la información que previamente se ha solicitado</p><p>Para <i>refrescar la cache</i>:</p><ul>' + '<li class="icon--windows"><strong> Windows:</strong><p>Presiona la combinación de teclas <span class="icon--fx--keycap">Ctrl</span> + <span class="icon--fx--keycap">⇧ Mayús</span> + <span class="icon--fx--keycap">R</span>, y <strong>vuelve a repetir la acción justo en el momento en el que lapágina comienza a cargarse</strong></p></li>' + '<li class="icon--mac"><strong> Mac:</strong><p>Presiona la combinación de teclas <span class="icon--fx--keycap">⌘ CMD</span> + <span class="icon--fx--keycap">⇧ Mayus.</span> + <span class="icon--fx--keycap">R</span>, y <strong>vuelve a repetir la acción justo en el momento en el que lapágina comienza a cargarse</strong></p></li>' + '<li class="icon--linux"><strong> Linux:</strong></strong><p>Presiona la combinación de teclas <span class="icon--fx--keycap">Ctrl</span> + <span class="icon--fx--keycap">⇧ Mayús</span> + <span class="icon--fx--keycap">R</span>, y <strong>vuelve a repetir la acción justo en el momento en el que lapágina comienza a cargarse</strong></p></li></ul><p><strong>¿Refrescar la caché ha solucionado el problema?</strong></p>',
+              'extData': 'dontLoad.html',
               'next': [{
-                  '_id': 'Support--End',
+                  '_id': 'end',
                   'class': 'btn--primary',
                   'icon': 'icon--ok icon--big',
                   'response': 'Si',
               }, {
-                  '_id': 'Support--DontLoad--School',
+                  '_id': 'dontLoadSchool',
                   'class': 'btn--primary btn--no',
                   'icon': 'icon--no icon--big',
                   'response': 'No',
               }]
           }, {
-              '_id': 'Support--DontLoad--School',
+              '_id': 'dontLoadSchool',
+              'title': '¿Estás en un centro educativo o en alguna infraestructura que pueda estar bajo un proxy?',
+              'extData': 'dontLoadSchool.html',
+              'next': [{
+                  '_id': 'end',
+                  'class': 'btn--primary',
+                  'icon': 'icon--ok icon--big',
+                  'response': 'Si',
+              }, {
+                  '_id': 'tetering',
+                  'class': 'btn--primary btn--no',
+                  'icon': 'icon--no icon--big',
+                  'response': 'No',
+              }]
+          }, {
+              '_id': 'tetering',
+              'title': 'Prueba con el tetering del móvil',
+              'data': '<p>Activando la opción de tetering de su móvil, y compartiendo la conexión con su computadora, puede comprobar si carga Bitbloq desde una red diferente.</p><p>Si consigue cargar, tiene un problema en la configuración de su red y/o software ajeno a Bitbloq; contacte con los administradores de la red.</p><p><strong>¿Has solucionado el problema?</strong></p>',
+              'next': [{
+                  '_id': 'end',
+                  'class': 'btn--primary',
+                  'icon': 'icon--ok icon--big',
+                  'response': 'Si',
+              }, {
+                  '_id': 'form',
+                  'class': 'btn--primary btn--no',
+                  'icon': 'icon--no icon--big',
+                  'response': 'No',
+              }]
+          }, {
+              '_id': 'w2b',
               'title': '',
-              'data': '',
               'next': []
           }, {
-              '_id': 'Support--W2B',
+              '_id': 'hardware',
               'title': '',
-              'data': '',
-              'next': []
-          }, {
-              '_id': 'Support--Hardware',
-              'title': '',
-              'data': '',
               'next': []
           }];
 
-        var currentId = ($routeParams.id !== undefined) ? $routeParams.id : 'Support--Index'
+        var cardIndex = db.filter(function(card) {
+            return card.default === 'index'
+          }).pop()
+        var cardEnd = db.filter(function(card) {
+            return card.default === 'end'
+          }).pop()
+        var card404 = db.filter(function(card) {
+            return card.default === '404'
+          }).pop()
+
+        var currentId = ($routeParams.id !== undefined) ? $routeParams.id : cardIndex._id
 
         $scope.card = db.filter(function(card) {
           return card._id === currentId
         }).pop()
-        console.log('card =', $scope.card)
         if ($scope.card === undefined || $scope.card.title === '') {
-          $scope.card = db.filter(function(card) {
-            return card._id === 'Support--404'
-          }).pop()
-          currentId = 'Support--404'
+          $scope.card = card404
+          currentId = card404._id
         }
 
-        $scope.showBack = !['Support--End', 'Support--Index', 'Support--404'].includes(currentId)
-
         $scope.go = function (childId) {
-          if (childId && childId !== 'Support--Index') {
+          if (childId && childId !== cardIndex._id) {
             common.supportSteps.push(childId)
             $location.path('/support/' + childId)
           } else {
@@ -128,6 +167,4 @@ angular.module('bitbloqApp')
             $location.path('/support')
           }
         }
-
-        console.log(common.supportSteps);
     });
