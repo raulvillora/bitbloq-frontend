75cac7a90e78bee29f8ca2d8bac07c35d3c3e801
Alvaro Almendros Gala <alvaroalmendros@me.com>
2017-10-16 15:24:45 +0200

    [SWBIT-2546] fix: support pane + icons

--- app/scripts/controllers/support.js
+++ app/scripts/controllers/support.js
@@ -75,7 +75,7 @@ angular.module('bitbloqApp')
           }, {
               '_id': 'Support--DontLoad',
               'title': '¿Tienes problemas cargando la web de Bitbloq?',
-              'data': '<p>¿Has <strong>refrescado la caché</strong> del navegador?</p><p>La caché del navegador es una función que tienen los principales navegadores web, que permite cargar las páginas web más rápido guardando en el ordenador parte de la información que previamente se ha solicitado</p><p>Para <i>refrescar la cache</i>:</p><ul>' + '<li class="icon--windows"><strong> Windows:</strong><p>Presiona la combinación de teclas <span class="icon--fx--keycap">Ctrl</span> + <span class="icon--fx--keycap">⇧ Mayús</span> + <span class="icon--fx--keycap">R</span>, y <strong>vuelve a repetir la acción justo en el momento en el que lapágina comienza a cargarse</strong></p></li>' + '<li class="icon--mac"><strong> Mac:</strong><p>Presiona la combinación de teclas <span class="icon--fx--keycap">⌘ CMD</span> + <span class="icon--fx--keycap">⇧ Mayus.</span> + <span class="icon--fx--keycap">R</span>, y <strong>vuelve a repetir la acción justo en el momento en el que lapágina comienza a cargarse</strong></p></li>' + '<li class="icon--linux"><strong> Linux:</strong></strong><p>Presiona la combinación de teclas <span class="icon--fx--keycap">Ctrl</span> + <span class="icon--fx--keycap">⇧ Mayús</span> + <span class="icon--fx--keycap">R</span>.<br>También puedes probar a presionar la tecla <span class="icon--fx--keycap">⇧ Mayús.</span> y haz clic en el botón “Recargar esta página” en la barra de herramientas de navegación.</p></li></ul><p><strong>¿Refrescar la caché ha solucionado el problema?</strong></p>',
+              'data': '<p>¿Has <strong>refrescado la caché</strong> del navegador?</p><p>La caché del navegador es una función que tienen los principales navegadores web, que permite cargar las páginas web más rápido guardando en el ordenador parte de la información que previamente se ha solicitado</p><p>Para <i>refrescar la cache</i>:</p><ul>' + '<li class="icon--windows"><strong> Windows:</strong><p>Presiona la combinación de teclas <span class="icon--fx--keycap">Ctrl</span> + <span class="icon--fx--keycap">⇧ Mayús</span> + <span class="icon--fx--keycap">R</span>, y <strong>vuelve a repetir la acción justo en el momento en el que lapágina comienza a cargarse</strong></p></li>' + '<li class="icon--mac"><strong> Mac:</strong><p>Presiona la combinación de teclas <span class="icon--fx--keycap">⌘ CMD</span> + <span class="icon--fx--keycap">⇧ Mayus.</span> + <span class="icon--fx--keycap">R</span>, y <strong>vuelve a repetir la acción justo en el momento en el que lapágina comienza a cargarse</strong></p></li>' + '<li class="icon--linux"><strong> Linux:</strong></strong><p>Presiona la combinación de teclas <span class="icon--fx--keycap">Ctrl</span> + <span class="icon--fx--keycap">⇧ Mayús</span> + <span class="icon--fx--keycap">R</span>, y <strong>vuelve a repetir la acción justo en el momento en el que lapágina comienza a cargarse</strong></p></li></ul><p><strong>¿Refrescar la caché ha solucionado el problema?</strong></p>',
               'next': [{
                   '_id': 'Support--End',
                   'class': 'btn--primary',
@@ -106,16 +106,20 @@ angular.module('bitbloqApp')
 
         var currentId = ($routeParams.id !== undefined) ? $routeParams.id : 'Support--Index'
 
-        $scope.card = db.filter(card => card._id === currentId).pop()
+        $scope.card = db.filter(function(card) {
+          return card._id === currentId
+        }).pop()
         console.log('card =', $scope.card)
         if ($scope.card === undefined || $scope.card.title === '') {
-          $scope.card = db.filter(card => card._id === 'Support--404').pop()
+          $scope.card = db.filter(function(card) {
+            return card._id === 'Support--404'
+          }).pop()
           currentId = 'Support--404'
         }
 
         $scope.showBack = !['Support--End', 'Support--Index', 'Support--404'].includes(currentId)
 
-        $scope.go = (childId) => {
+        $scope.go = function (childId) {
           if (childId && childId !== 'Support--Index') {
             common.supportSteps.push(childId)
             $location.path('/support/' + childId)
